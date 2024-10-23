8cb996be21373f205852f695998dc00599931bdc
Johan  Antonio <147890607+johansitoweb@users.noreply.github.com>
2024-10-23 10:59:59 -0400

    Add files via upload

---
+++ appconsola/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// creare una app por consola la cual mostrara informacion de dos productos techologicos
+
+string NpombrePROducto = "computdora" ;
+string NombreSegundoPROducto = "app android veterinaria";
+Console.WriteLine("Informacion de los prodctos informaticos" + NpombrePROducto + NombreSegundoPROducto);
+Console.Write(Console.ReadLine());
